2cdd2688cac04375782a75e5a5fb9dc045741fb6
rogeriodelphi <rogeriodelphi@gmail.com>
2020-09-12 02:13:39 -0300

    Mise en place du projet Django - Changer la langue de l'administration

--- DashboardDevise/settings.py
+++ DashboardDevise/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
@@ -114,7 +114,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript,LANGUA Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
