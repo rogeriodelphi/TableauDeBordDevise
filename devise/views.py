fab7db3b62651facd0d1589c0a855e0528a8c83a
rogeriodelphi <rogeriodelphi@gmail.com>
2020-09-13 01:32:31 -0300

    Création de l'application devise - Envoyer des informations avec le context

--- devise/views.py
+++ devise/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def dashboard(request):
-    return render(request, "devise/index.html")
+    return render(request, "devise/index.html", context={"liste_nombre": range(10)})
 
