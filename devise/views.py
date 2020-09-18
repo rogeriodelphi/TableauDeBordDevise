4ab731133ace5653f9e5a38369f5f74fa4784aae
rogeriodelphi <rogeriodelphi@gmail.com>
2020-09-18 02:02:38 -0300

    Utilisation de l'API - Modifier les jours avec l'URL

--- devise/views.py
+++ devise/views.py
@@ -4,12 +4,13 @@ from django.shortcuts import render, HttpResponse
 import api
 
 # Create your views here.
-def dashboard(request):
-    days, rates = api.get_rates(currencies=["USD"], days=30)
+def dashboard(request, days_range=60, currencies="CAD"):
+    days, rates = api.get_rates(currencies=currencies.split(","), days=days_range)
 
-    return render(request, "devise/index.html", context={"data": rates["USD"],
+    return render(request, "devise/index.html", context={"data": rates,
                                                          "days_labels": days})
 
-
+    print(data)
+    print(rates)
 # rates = taxas
 # days =  dias
